45c0f74f1b27eeeb36b36f95753f94956d94cb80
Chris Moreton <chris.moreton@syscad.net>
2006-04-18 18:30:17 +0000

    Admin

---
+++ bin/RegAll.cmd
@@ -0,0 +1,12 @@
+regsvr32 /s ScdCOM.dll
+regsvr32 /s ScdVB.dll
+regsvr32 /s ScdIF.dll
+regsvr32 /s ScdMdl.dll
+regsvr32 /s ScdSlv.dll
+regsvr32 /s SysCADMarshal_ps.dll
+
+ScdIODB       /regserver
+SysCADMarshal /regserver
+SysCAD        /regserver
+
+rem pause
