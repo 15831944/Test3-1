fcdde927dbe434d94b806d8065e71baa68c061ff
Chris Moreton <chris.moreton@syscad.net>
2006-04-20 15:27:52 +0000

    Correct the BYPASSLICENSING flag for VS6

--- Common/Scd/ScdLib/ScdVerLic.h
+++ Common/Scd/ScdLib/ScdVerLic.h
@@ -8,6 +8,8 @@
 //  License Options
 //===========================================================================
 
+#if _MSC_VER>=1400
+
 #ifdef _DEBUG
 #define BYPASSLICENSING       01
 #elif  _RELEASE
@@ -17,6 +19,18 @@
 #endif
 #define ALLOWCNMVERSION       01
 
+#else
+
+#ifdef _DEBUG
+#define BYPASSLICENSING       01
+#elif  _RELEASE
+#define BYPASSLICENSING       0
+#else
+#define BYPASSLICENSING       01
+#endif
+#define ALLOWCNMVERSION       01
+
+#endif
 //===========================================================================
 //
 //===========================================================================
