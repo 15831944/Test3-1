b51bc08ea76d0f9c7481a542ede526a29fbb61d1
Keith Armstrong <keith.armstrong@syscad.net>
2006-09-22 08:40:20 +0000

    Admin

--- Common/Scd/ScdLib/RevHist.h
+++ Common/Scd/ScdLib/RevHist.h
@@ -21,7 +21,7 @@
 #define SCD_PATCHNOTE    "Update 2"   /* Update/patch number/comment*/
 
 //#define SCD_PATCHDATE    ""        /* Update/patch release date*/
-#define SCD_PATCHDATE    "20 September 2006"
+#define SCD_PATCHDATE    "22 September 2006"
 #define SCD_COMPILEDATE    __DATE__
 
 #endif // __REVHIST_H
@@ -2567,10 +2567,11 @@ Update 1 : 15 September 2006
 4) (#555) Fix for crash during load due to bad connections.
 5) (#557,559) Thickener and washer improvements for dynamic transfer mode.
 
-Update 2 : 20 September 2006
+Update 2 : 22 September 2006
 ------------------------------
 1) (#551,556) Improvements for archive reporter.
 2) (#528,565) Improved tag checking of specie tags and phase names.
+3) (#569,570,571) Makeup block improvements.
 
 =============================================================================
 ================                SysCAD 9.2                   ================
