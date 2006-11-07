9b8d6d8ee381f816ae48b44026dc2a5c043d85fb
Keith Armstrong <keith.armstrong@syscad.net>
2006-11-07 03:36:48 +0000

    Admin, etc

--- Common/Scd/ScdLib/RevHist.h
+++ Common/Scd/ScdLib/RevHist.h
@@ -21,7 +21,7 @@
 #define SCD_PATCHNOTE    "Update 2"   /* Update/patch number/comment*/
 
 //#define SCD_PATCHDATE    ""        /* Update/patch release date*/
-#define SCD_PATCHDATE    "5 November 2006"
+#define SCD_PATCHDATE    "6 November 2006"
 #define SCD_COMPILEDATE    __DATE__
 
 #endif // __REVHIST_H
