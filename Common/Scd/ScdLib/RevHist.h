0392ae77a32ca04051f1d359ca657a59cddcd174
Keith Armstrong <keith.armstrong@syscad.net>
2007-02-06 03:13:46 +0000

    #633 Archive time options (part 4)

--- Common/Scd/ScdLib/RevHist.h
+++ Common/Scd/ScdLib/RevHist.h
@@ -2708,11 +2708,11 @@ Build 122 : 16 November 2006
 1399  4/02/07: (#623) Extend PGM TextLabel(..) command to work in class definitions.
 1402  4/02/07: Minor client add-on DLL fixes.
 1407  5/02/07: (#721) Added evaporation mode option to shell and tube heater.
-1410  6/02/07: (#633) Added separate archive report time fields for date and time.
+1411  6/02/07: (#633) Added separate archive report time fields for date and time.
 
 ------------------------------
 //Latest release information:*/
-#define SCD_PATCH_SVN    1410
+#define SCD_PATCH_SVN    1411
 #define SCD_PATCHDATE    SCD_COMPILE_DT
 
 /*
